f6c3e62a4f7e1196f3c996cc9f6a358b71443c81
Fanuel <riquezerofive@gmail.com>
2018-12-04 16:27:52 -0200

    Projeto criado

---
+++ src/main/java/com/fhdev/aebeauty/AebeautyApplication.java
@@ -0,0 +1,12 @@
+package com.fhdev.aebeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AebeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AebeautyApplication.class, args);
+	}
+}
